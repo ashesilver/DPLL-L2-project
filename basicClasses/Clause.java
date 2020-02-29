195f69f55e9d62ba07ab9e1ced9cd6545438a99d
Guidyous <guillaume.hakenholz@gmail.com>
2020-02-29 12:56:17 +0100

    Création de la classe <<Litéral>>

--- basicClasses/Clause.java
+++ basicClasses/Clause.java
@@ -1,17 +1,25 @@
 package basicClasses;
 
+import java.util.ArrayList;
+
 public class Clause
 {
 	//Atributs
-	Form e;
+	ArrayList<Literal> literaux;
 
 	//Constructeur
-	public Clause(Form e)
+	public Clause()
 	{
-		this.e = e;
+		this.literaux = new ArrayList<>();
 	}
 
 	//Méthodes
+
+	//TODO faire une méthode ajouter qui ajoute un element literal dans la liste de literaux
+
+	//TODO faire une méthode tautologie qui rend true si il y a une tautologie dans la clause
+
+	
 	public String toString()
 	{
 		String clause = new String(e.toString());
---
+++ basicClasses/Literal.java
@@ -0,0 +1,13 @@
+package basicClasses;
+
+public class Literal
+{
+	//Attributs
+	private Form e;
+
+	//Constructeur
+	public Literal(Form e)
+	{
+		this.e = e;
+	}
+}
